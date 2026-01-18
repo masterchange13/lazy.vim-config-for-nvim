8df897fa911a1ba1657be2abb77de1b2599496f0
xiaoxin <3115242940@qq.com>
2026-01-18 14:50:42 +0800

    2026-1-18

--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -1,72 +1,119 @@
--- 自动安装packer
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+-- lua/plugins.lua
+
+-- 1. 自动安装 lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+-- 2. 插件列表
+require("lazy").setup({
 
--- 保存此文件自动更新安装软件
--- 注意PackerCompile改成了PackerSync
--- plugins.lua改成了plugins-setup.lua，适应本地文件名字
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
-  augroup end
-]])
+  -- 🎨 主题
+  { "scottmckendry/cyberdream.nvim" },
 
-return require('packer').startup(function(use)
-  -- use 'wbthomason/packer.nvim'
-  -- use 'folke/tokyonight.nvim' -- 主题
-  use { "scottmckendry/cyberdream.nvim" }
-  use {
-    'nvim-lualine/lualine.nvim',  -- 状态栏
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }  -- 状态栏图标
-  }
-  use {
-    'nvim-tree/nvim-tree.lua',  -- 文档树
-    requires = {
-      'nvim-tree/nvim-web-devicons', -- 文档树图标
-    }
-  }
-  use "christoomey/vim-tmux-navigator" -- 用ctl-hjkl来定位窗口
-  use "nvim-treesitter/nvim-treesitter" -- 语法高亮
---  use {
---    "HiPhish/nvim-ts-rainbow2",
---    requires = { "nvim-treesitter/nvim-treesitter" },
---  }
-  use {
+  -- 📊 状态栏
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  -- 🌲 文件树
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  -- tmux 窗口跳转
+  { "christoomey/vim-tmux-navigator" },
+
+  -- 🌈 Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+
+  -- 🧱 LSP
+  {
     "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",  -- 这个相当于mason.nvim和lspconfig的桥梁
-    "neovim/nvim-lspconfig"
-  }
-    -- 自动补全
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "L3MON4D3/LuaSnip" -- snippets引擎，不装这个自动补全会出问题
-  use "saadparwaiz1/cmp_luasnip"
-  use "rafamadriz/friendly-snippets"
-  use "hrsh7th/cmp-path" -- 文件路径
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+  },
+  {
+    "neovim/nvim-lspconfig",
+  },
 
-  use "numToStr/Comment.nvim" -- gcc和gc注释
-  use "windwp/nvim-autopairs" -- 自动补全括号
+  -- 🤖 自动补全
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
 
-  use "akinsho/bufferline.nvim" -- buffer分割线
-  use "lewis6991/gitsigns.nvim" -- 左则git提示
+  -- 💬 注释
+  { "numToStr/Comment.nvim", opts = {} },
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',  -- 文件检索
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+  -- 🔧 括号自动补全
+  { "windwp/nvim-autopairs", opts = {} },
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+  -- 📑 buffer 栏
+  { "akinsho/bufferline.nvim" },
+
+  -- 🌱 Git 提示
+  { "lewis6991/gitsigns.nvim" },
+
+  -- 🔍 Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.1",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  -- leetcode
+  {
+    "kawre/leetcode.nvim",
+    -- build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    dependencies = {
+        -- include a picker of your choice, see picker section for more details
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+    },
+    opts = {
+        -- configuration goes here
+        ---@type string
+      arg = "leetcode.nvim",
+
+      ---@type lc.lang
+      lang = "cpp",
+
+      cn = { -- leetcode.cn
+          enabled = true, ---@type boolean
+          translator = true, ---@type boolean
+          translate_problems = true, ---@type boolean
+      },
+
+      ---@type lc.storage
+      storage = {
+          home = vim.fn.stdpath("data") .. "/leetcode",
+          cache = vim.fn.stdpath("cache") .. "/leetcode",
+      },
+    },
+  },
+
+})
+
